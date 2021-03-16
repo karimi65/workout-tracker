e3685016b23ca8b9a2f52db5beed678e40814022
Captain <71038018+karimi65@users.noreply.github.com>
2021-03-15 22:01:36 -0500

    Add api routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,6 +1,37 @@
 const router = require('express').Router()
 const workout = require('../models/Exercise')
 
+router.get('/api/workouts', (req, res) => {
+    workout.find({})
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => { res.json(err) })
+})
+
+router.get('/api/workouts/range', (req, res) => {
+    workout.find({}).limit(7)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => { res.json(err) })
+})
+
+router.post('/api/workouts', (req, res) => {
+    workout.create(req.body)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => { res.json(err) })
+})
+
+router.put('/api/workouts/:id', (req, res) => {
+    workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => { res.json(err) })
+})
 
 
 module.exports = router
